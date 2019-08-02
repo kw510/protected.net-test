67e8f7a232da4d36e731632383b1d98aa0260fa8
Ashley Ktorou <ashley.ktorou@justdevelop.it>
2019-08-02 15:56:58 +0100

    add comments to component

--- src/Components/ExampleComponent/ExampleComponent.php
+++ src/Components/ExampleComponent/ExampleComponent.php
@@ -13,31 +13,51 @@ class ExampleComponent extends AbstractComponent
     $this->_setPrimaryAttributes();
   }
 
-  /** @return string */
+  /**
+   * This is used to find the view
+   *
+   * @return string
+   */
   protected function _getTemplateClass(): string
   {
     return self::class;
   }
 
-  /** @return string */
+  /**
+   * All our styling used BEM, the name you enter here is the root name for BEM styling
+   *
+   * @return string
+   */
   public function getBlockName(): string
   {
     return 'example-component';
   }
 
-  /** @return $this */
+  /**
+   * Used to set the blockname as an attribute on the component
+   * This is used for linking it to typescript component of the same nam
+   *
+   * @return $this
+   */
   protected function _setPrimaryAttributes()
   {
     return $this->setAttribute($this->getBlockName(), true);
   }
 
-  /** @return $this */
+  /**
+   * We use static i() methods to instantiate as it's shorter and easier to chain
+   *
+   * @return $this
+   */
   public static function i()
   {
     return new static();
   }
 
   /**
+   * The resource manager is used to load any resources that aren't being included by default
+   * This is normally anything that's compiled local to the component or partial
+   *
    * @param ResourceManager $manager
    *
    * @throws Exception
